3b95462a22d29429db99a0e6f92c26ace7ed0427
LUCAS RUFATO TODESCO <39990609802@info.local>
2026-09-08 14:55:12 -0300

    login finalizado

---
+++ login/login.js
@@ -0,0 +1,21 @@
+
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+console.log(btnSenha.type)
+
+btnSenha.addEventListener("click", function() {
+
+    
+
+    // if(campoSenha.type == "password") {
+    //     campoSenha.type = "text";
+    // }
+
+    // else{
+    //     campoSenha.type = "password";
+    // }
+
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password"
+
+})
